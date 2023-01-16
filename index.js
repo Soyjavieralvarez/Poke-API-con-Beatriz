cf09b244ecc7437d6f1fc0ecaad9eced14366b4c
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-16 14:19:02 +0100

    submit 4

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-//? 1º paso, traer los personajes de Pokemon
+const iconsTypes = [
+  "bug",
+  "dragon",
+  "electric",
+  "fairy",
+  "fighting",
+  "fire",
+  "flying",
+  "ghost",
+  "grass",
+  "ground",
+  "ice",
+  "normal",
+  "poison",
+  "psychic",
+  "rock",
+  "steel",
+  "water",
+];
 
 let characters = [];
 let charactersDetails = []; //?Aquí guardo el array con la info, abilities, name,id...
@@ -6,49 +24,44 @@ const boxPokemons = document.createElement("div");
 const buscar$$ = document.querySelector(".buscar");
 
 const search = () => {
-
- const pokemonsFiltered = charactersDetails.filter((character) => {
-    console.log(character.name)
-    return character.name
-    .toLowerCase()
-    .includes(buscar$$.value.toLowerCase())
- })
-pintar(pokemonsFiltered);
+  const pokemonsFiltered = charactersDetails.filter((character) => {
+    console.log(character.name);
+    return character.name.toLowerCase().includes(buscar$$.value.toLowerCase());
+  });
+  pintar(pokemonsFiltered);
 };
 buscar$$.addEventListener("input", search);
 
-
-
 const pintar = (charactersDetails) => {
-    boxPokemons.innerHTML = "";
-//   console.log(charactersDetails);
-// let types = [];
-  for (const character of charactersDetails) {
-//    console.log(character.sprites.other.dream_world.front_default) 
-    // for (const type of character.types) {
-    // //   console.log(character.types);
-    //   types.push(type.type.name);
-    // }
+  boxPokemons.innerHTML = "";
 
-    const carta$$   = document.createElement("div");
-    const image$$   = document.createElement("img");
-    const id$$      = document.createElement("h3");
-    const name$$    = document.createElement("h4");
-    const height$$  = document.createElement("p");
-    const specie$$  = document.createElement("p");
-    const types$$   = document.createElement("p");
-    const weight$$  = document.createElement("p");
+  for (const character of charactersDetails) {
+    const carta$$ = document.createElement("div");
+    const image$$ = document.createElement("img");
+    const id$$ = document.createElement("h2");
+    const name$$ = document.createElement("h4");
+    const height$$ = document.createElement("p");
+    const specie$$ = document.createElement("p");
+    const types$$ = document.createElement("p");
+    const weight$$ = document.createElement("p");
 
     boxPokemons.classList.add("boxPokemons");
     carta$$.classList.add("carta");
+    image$$.classList.add("img");
+    id$$.classList.add("h2");
+    name$$.classList.add("h4");
+    height$$.classList.add("p");
+    specie$$.classList.add("p");
+    types$$.classList.add("p");
+    weight$$.classList.add("p");
 
-    image$$.src          = character.sprites.other.dream_world.front_default;
-    id$$.textContent     = ("Id: ") + character.id;
-    name$$.textContent   = character.name;
-    height$$.textContent = character.height;
-    specie$$.textContent = character.specie;
-    types$$.textContent  = character.type;
-    weight$$.textContent = character.weight;
+    image$$.src = character.sprites.other.dream_world.front_default;
+    id$$.textContent = "Id: " + character.id;
+    name$$.textContent = character.name;
+    height$$.textContent = "Height: " + character.height;
+    specie$$.textContent = "Specie : " + character.specie;
+    types$$.textContent = "Type: " + character.type;
+    weight$$.textContent = "Weight: " + character.weight;
 
     carta$$.append(
       name$$,
@@ -70,14 +83,11 @@ const getPokemons = async () => {
   );
   const result = await reply.json();
   characters = [...result.results];
-//   console.log(charactersDetails)
-
 
   for (const character of characters) {
     const charactersInfo = await fetch(character.url);
     const characterResults = await charactersInfo.json();
 
-    // console.log(characterResults);
     charactersDetails.push(characterResults);
   }
   pintar(charactersDetails);
